66abeb0d13df468f29fd7f64cce210abd1b22a2e
mateusbertolo <mateuscampos72@gmail.com>
2024-04-16 19:09:13 -0300

    feat(button)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
 import { Button } from './components/common/Button'
-import { ButtonVariant } from './components/common/Button/button.constant'
+import { ButtonVariant } from './components/common/Button/Button.constants'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    {count}
-    <Button variant= {ButtonVariant.Default} onClick={() => setCount(count + 1)}>Quero adotar</Button>
-    <Button variant= {ButtonVariant.Disabled}onClick={() => setCount(count + 1)}>Quero adotar</Button>
+      <Button
+        variant={ButtonVariant.Default}
+        onClick={() => setCount(count + 1)}
+      >
+        Quero adotar
+      </Button>
+      <Button
+        variant={ButtonVariant.Outlined}
+        onClick={() => setCount(count + 1)}
+      >
+        Tenho um abrigo
+      </Button>
     </>
   )
 }
