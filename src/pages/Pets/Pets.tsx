5c0269a6311b6e3f20fa3f47e4530ccd385886ef
mateusbertolo <mateuscampos72@gmail.com>
2024-04-18 20:11:00 -0300

    feat(18-04)

--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -1,45 +1,33 @@
-
- import { useEffect } from "react";
+import { useEffect } from "react";
 import { Header } from "../../components/common/Header/Header";
 import { Grid } from "../../components/layout/Grid";
-import styles from './Pets.module.css'
+import styles from './Pets.module.css';
 import { Card } from "../../components/common/Card";
 import { Skeleton } from "../../components/common/skeleton";
 import { useQuery } from "@tanstack/react-query";
 import { GetPets } from "../../services/pets/getPets";
 
-export function Pets(){
-const {data, isLoading} = useQuery({
-    queryKey: ['get-pets'],
-    queryFn: () => GetPets(),
+export function Pets() {
+    const { data, isLoading } = useQuery({
+        queryKey: ['get-pets'],
+        queryFn: () => GetPets(),
+    });
 
-})
+    useEffect(() => {
 
-export function Pets(){
-    useEffect(() =>{
+    }, []);
 
-    }, [])
-    return(
+    return (
         <Grid>
             <div className={styles.container}>
-
-            <Header />
-            {
-                isLoading && (<Skeleton containerClassName={styles.skeleton} count={10}/>
-            )}
-            <main className={styles.list}>
-                {data?.items?.map(Pets) => (
-                <Card href="pets/1" text="Bonny" thumb=""/>
-                <Card href="pets/2" text="Nina" thumb=""/>
-                <Card href="pets/3" text="Julia" thumb=""/>    
-                <Card href="pets/2" text="Nina" thumb=""/>
-                <Card href="pets/3" text="Julia" thumb=""/>    
-                <Card href="pets/2" text="Nina" thumb=""/>
-                <Card href="pets/3" text="Julia" thumb=""/>
-                )}          
-            </main>
+                <Header />
+                {isLoading && <Skeleton containerClassName={styles.skeleton} count={10} />}
+                <main className={styles.list}>
+                    {data?.items?.map((pet) => (
+                        <Card key={pet.id} href={`pets/${pet.id}`} text={pet.name} thumb={pet.thumb} />
+                    ))}
+                </main>
             </div>
-
         </Grid>
-    )
+    );
 }
