6d02b2339009d4a3b11ded8e4c67c692d5a86714
mateusbertolo <mateuscampos72@gmail.com>
2024-04-17 21:31:02 -0300

    feat(apiconnect)

--- src/services/api/Pets/getPets.ts
+++ src/services/api/Pets/getPets.ts
@@ -1,13 +1,16 @@
 import axios from "axios"
 import httpClient from "./httpClient"
+import { GetPetsRequet } from "../../../interfaces/Pets"
 
 
-export async function getPets(){
+export async function getPets(Params:GetPetsRequet:Promise<GetPetsResponse>){
     try{
         const response = await httpClient.get('/Pets')
         return response.data
     }catch{Error}{
-
+        console.log'Erro ao cadastrar Pets', console.error();
+        throw.error
+        
     }
 
 
