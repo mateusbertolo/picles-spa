07eb528ee61ef23789498c2846e53395a896ce8e
mateusbertolo <mateuscampos72@gmail.com>
2024-04-19 20:30:55 -0300

    feat(search)

--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -1,31 +1,42 @@
+import { useEffect, useState } from 'react'
 import { Header } from '../../components/common/Header'
 import { Grid } from '../../components/layout/Grid'
 import styles from './Pets.module.css'
 import { Card } from '../../components/common/Card'
 import { getPets } from '../../services/pets/getPets'
-import { Skeleton } from '../../components/common/Skeleton'
+import { Skeleton } from '../../components/common/skeleton'
 import { useQuery } from '@tanstack/react-query'
-import { Pagination } from '../../components/common/Pagination'
-import { useSearchParams } from 'react-router-dom'
+import { Select } from '../../components/common/Select'
 
 export function Pets() {
-  const [searchParams, setSearchParams] = useSearchParams()
+  const { data, isLoading } = useQuery({
+    queryKey: ['get-pets'],
+    queryFn: () => getPets(),
+  })
 
-  const urlParams = {
-    page: searchParams.get('page') ? Number(searchParams.get('page')) : 1,
-  }
-
-  function changePage(page: number) {
-    setSearchParams((params) => {
-      params.set('page', String(page))
-      return params
-    })
-  }
+const urlParams={
+  page
+}
 
   return (
     <Grid>
       <div className={styles.container}>
         <Header />
+        <form className={styles.filters}>
+            <div className={styles.columns}>
+                <div className={styles.columns}>
+                    <Select
+                        label="Especie"
+                        options={[
+                            { value: '', text: 'todos' },
+                            { value: 'cachorro', text: 'cachorro' },
+                            { value: 'gato', text: 'gato' },
+                        ]}
+                    />
+                </div>
+            </div>
+            <button type='submit'>Buscar</button>
+        </form>
         {isLoading && (
           <Skeleton containerClassName={styles.skeleton} count={10} />
         )}
@@ -39,13 +50,6 @@ export function Pets() {
             />
           ))}
         </main>
-        {data?.currentPage && (
-          <Pagination
-            currentPage={data.currentPage}
-            totalPages={data.totalPages}
-            onPageChange={(number) => changePage(number)}
-          />
-        )}
       </div>
     </Grid>
   )
