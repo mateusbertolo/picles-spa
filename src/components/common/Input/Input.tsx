8e1eb76b79e004f72a1961878ca69b756c98fcac
mateusbertolo <mateuscampos72@gmail.com>
2024-04-23 22:20:03 -0300

    feat(admin painel)

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,6 +1,18 @@
-export function Input(){
+import styles from './Input.module.css'
+import { InputHTMLAttributes, Ref, forwardRef } from 'react'
+
+interface IInput extends
+InputHTMLAttributes<HTMLInputElement>{
+    label:string
+}
+
+export function ForwardInput({label,...rest}: IInput, ref: Ref<HTMLInputElement>){
     return(
-        <>
-        </>
+        <div className={styles.InputGroup}>
+            <label>label</label>
+            <input ref={ref}{...rest}/>
+        </div>
     )
 }
+
+export const Input = forwardRef(ForwardInput)
