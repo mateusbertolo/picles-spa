5efb82ea0683dd631407bda8777b85b78879821c
mateusbertolo <mateuscampos72@gmail.com>
2024-04-23 21:36:41 -0300

    feat(admin painel)

--- src/components/common/authHOC/authHOC.tsx
+++ src/components/common/authHOC/authHOC.tsx
@@ -1,16 +1,14 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet, useOutlet } from "react-router-dom";
 import { useShelter } from "../../../hooks/useShelter";
-
-export function Sidebar(){
-    const [data] = useShelter()
-}
 
 export function AuthHOC() {
     const { data, isLoading } = useShelter();
     const canAccess = !!data?.shelterWhatsApp;
 
     if (isLoading) return null;
-    if (!canAccess) return <Navigate to="/admin" />;
+    if (!canAccess) return <Navigate to="/admin" />
+    return <Outlet/>
+
 
     // Aqui você pode retornar o conteúdo que deseja renderizar caso o usuário tenha acesso
 }
